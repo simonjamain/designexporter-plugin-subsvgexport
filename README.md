dfa57494c0174014edc4a8f9f700a80d5eadaf36
Simon Jamain <jamain.simon@gmail.com>
2016-10-27 16:40:33 +0200

    first working version

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # designexporter-plugin-subsvgexport
-A plugin to export identified objects with inkscape
+A plugin to export identified objects to png with inkscape
+
+config file goes like this:
+```yaml
+subsvgexport:
+  - source: path of source file, absolute or relative to conf file
+    objects:
+      - id: css id (inkscape id)
+        dpi: 300 #for example
+        only: true #enable "--export-id-only" inkscape flag
+        output: path to output file
+```
---
+++ exportsubsvg.rb
@@ -0,0 +1,16 @@
+require 'nokogiri'
+require 'fileutils'
+require 'shellwords'
+require 'tempfile'
+require 'byebug'
+
+def exportsubsvg (sourceFileName, object)
+  command = "inkscape --export-id=#{object['id']}#{' --export-id-only' if object['only']} --export-dpi=#{object['dpi']} --export-png=#{object['output'].shellescape} #{sourceFileName.shellescape}"
+
+  # ensure path exists
+  FileUtils.mkpath(File.dirname(object['output']))
+
+  #convert svg to pdf
+  print "#{object['output']} produced.\n" if system command
+
+end
