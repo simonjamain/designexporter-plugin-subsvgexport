40b482e502317365601eb934eee3725d7d6c4a46
Simon Jamain <jamain.simon@gmail.com>
2016-11-04 16:23:11 +0100

    removed 'only' option

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # designexporter-plugin-subsvgexport
-A plugin to export identified objects to png with inkscape
+A plugin to export identified objects to png with inkscape.
+Be carefull ot have the corresponding layers set to visible.
 
 config file goes like this:
 ```yaml
@@ -8,6 +9,5 @@ subsvgexport:
     objects:
       - id: css id (inkscape id)
         dpi: 300 #for example
-        only: true #enable "--export-id-only" inkscape flag
         output: path to output file
 ```
--- exportsubsvg.rb
+++ exportsubsvg.rb
@@ -4,12 +4,12 @@ require 'shellwords'
 require 'tempfile'
 
 def exportsubsvg (sourceFileName, object)
-  command = "inkscape --export-id=#{object['id']}#{' --export-id-only' if object['only']} --export-dpi=#{object['dpi']} --export-png=#{object['output'].shellescape} #{sourceFileName.shellescape}"
+  command = "inkscape --export-id=#{object['id']} --export-id-only --export-dpi=#{object['dpi']} --export-png=#{object['output'].shellescape} #{sourceFileName.shellescape}"
 
   # ensure path exists
   FileUtils.mkpath(File.dirname(object['output']))
 
   #convert svg to pdf
-  print "#{object['output']} produced.\n" if system command
+  print "#{object['output']} produced.\n" if system(command)
 
 end
